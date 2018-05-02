8525ea9fbfa908fd40b20b109aa6d9f3aaef8e00
HuangXin <hx1310320729@gmail.com>
2018-05-02 21:19:20 +0800

    登录
    登录

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,6 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {'CHARSET': 'utf-8',
+                      'COLLATION': None,
+                      'MIRROR': None,
+                      'NAME': None},
     }
 }
 
